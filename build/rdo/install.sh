5726c3ee86665320ece73d3db0fed5426da3c0de
wujiang <jiangwu1911@hotmail.com>
2015-04-18 09:28:20 +0800

    added a patch

--- build/rdo/install.sh
+++ build/rdo/install.sh
@@ -86,6 +86,14 @@ function post_install() {
     systemctl restart openstack-glance-api
 }
 
+function apply_patches() {
+    yum install -y patch
+    patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
+    pkill -9 dnsmasq
+    systemctl restart openstack-nova-network
+}
+
 pre_install
 install_openstack
 post_install
+apply_patches
