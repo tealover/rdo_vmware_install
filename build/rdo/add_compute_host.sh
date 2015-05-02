e20ea14bfc75cde8c4a4d7a7d9102a9464d9aebe
wujiang <jiangwu1911@hotmail.com>
2015-05-02 13:29:15 +0800

    Added pre and post install functions

--- build/rdo/add_compute_host.sh
+++ build/rdo/add_compute_host.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-CONTROLLER_HOST=192.168.145.23
-NEW_COMPUTE_HOST=192.168.145.24
+CONTROLLER_HOST=172.16.71.204
+NEW_COMPUTE_HOST=172.16.71.205
 VMWARE_CLUSTER_NAME=cluster02
+FIXED_IP_RANGE="10.0.0.0/16"
 
 answerfile="mystack.txt"
 
-cp $answerfile ${answerfile}_add_node
-sed -i "s/^CONFIG_COMPUTE_HOSTS.*$/&,$NEW_COMPUTE_HOST/" ${answerfile}_add_node
-sed -i "s/^CONFIG_NETWORK_HOSTS.*$/&,$NEW_COMPUTE_HOST/" ${answerfile}_add_node
-sed -i "s/^CONFIG_VCENTER_CLUSTER_NAME=.*/CONFIG_VCENTER_CLUSTER_NAME=$VMWARE_CLUSTER_NAME/" ${answerfile}_add_node
-sed -i "s/^EXCLUDE_SERVERS=.*/EXCLUDE_SERVERS=$CONTROLLER_HOST/" ${answerfile}_add_node
+function install_openstack() {
+    cp $answerfile ${answerfile}_add_node
+    sed -i "s/^CONFIG_COMPUTE_HOSTS.*$/&,$NEW_COMPUTE_HOST/" ${answerfile}_add_node
+    sed -i "s/^CONFIG_NETWORK_HOSTS.*$/&,$NEW_COMPUTE_HOST/" ${answerfile}_add_node
+    sed -i "s/^CONFIG_VCENTER_CLUSTER_NAME=.*/CONFIG_VCENTER_CLUSTER_NAME=$VMWARE_CLUSTER_NAME/" ${answerfile}_add_node
+    sed -i "s/^EXCLUDE_SERVERS=.*/EXCLUDE_SERVERS=$CONTROLLER_HOST/" ${answerfile}_add_node
 
-packstack --answer-file=${answerfile}_add_node
+    packstack --answer-file=${answerfile}_add_node
+}
+
+function pre_install() {
+    ssh-copy-id root@$NEW_COMPUTE_HOST
+}
+
+function post_install() {
+    ssh $NEW_COMPUTE_HOST "iptables -t nat -I POSTROUTING -s $FIXED_IP_RANGE ! -d $FIXED_IP_RANGE -j MASQUERADE; service iptables save"
+    ssh $NEW_COMPUTE_HOST "/root/rdo/apply_patch.sh"
+}
+
+pre_install
+install_openstack
+post_install
