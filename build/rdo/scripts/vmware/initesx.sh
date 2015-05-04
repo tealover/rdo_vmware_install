5692f624167f3e90e599ae0f7b74cf9a7ee43f8f
wujiang <jiangwu1911@hotmail.com>
2015-05-04 11:52:44 +0800

    ab`

--- build/rdo/scripts/vmware/initesx.sh
+++ build/rdo/scripts/vmware/initesx.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
-ESX_IP=192.168.1.109
-
-VCENTER_IP=192.168.1.107
+VCENTER_IP=192.168.206.140
 USERNAME=root
 PASSWORD=vmware
 
 VLAN_RANGE_START=101
 VLAN_RANGE_END=110
-NFS_SERVER_IP=192.168.1.102
+NFS_SERVER_IP=192.168.206.147
+NFS_SHARE="/mnt/vg01/vol01/share01 /mnt/vg01/vol02/share02"
+
+function exec_cmd() {
+    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP $@
+}
 
 function init_network() {
     for((i=$VLAN_RANGE_START;i<=$VLAN_RANGE_END;i++)); do
-        esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP network vswitch standard portgroup add -p br${i} -v vSwitch0
-        esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP network vswitch standard portgroup set -p br${i} -v $i
+        exec_cmd network vswitch standard portgroup add -p br${i} -v vSwitch0
+        exec_cmd network vswitch standard portgroup set -p br${i} -v $i
     done
 }
 
 function init_storage() {
-    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP storage nfs add --host $NFS_SERVER_IP --share /mnt/vg01/lv01/share01 --volume-name=share01
-    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP storage nfs add --host $NFS_SERVER_IP --share /mnt/vg01/lv02/share02 --volume-name=share02
-    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP storage nfs add --host $NFS_SERVER_IP --share /mnt/vg01/lv03/share03 --volume-name=share03
-
+    for i in $NFS_SHARE; do
+        vol=`basename $i`
+        exec_cmd storage nfs add --host $NFS_SERVER_IP --share /mnt/vg01/lv01/$i --volume-name=$vol
+        echo $i
+    done
     local_datastore=`esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP storage filesystem list | grep VMFS-5 | cut -f1 -d ' '`
-    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP storage filesystem unmount -p $local_datastore
+    exec_cmd storage filesystem unmount -p $local_datastore
 }
 
 function enable_vnc() {
     scp vnc.xml root@$ESX_IP:/etc/vmware/firewall
-    esxcli -s $VCENTER_IP -u $USERNAME -p $PASSWORD -h $ESX_IP network firewall refresh
+    exec_cmd network firewall refresh
 }
 
+if [ -z $1 ]; then
+    echo "Usage: $0 <esx_host_ip>"
+    exit 
+fi
+
+ESX_IP=$1
 init_network
 init_storage
 enable_vnc
