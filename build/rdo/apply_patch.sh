81f28969dffe733ae5f3fd9f2a2f001f33fca774
inchin <pyz007@gmail.com>
2015-07-09 14:46:36 +0800

    modify source code to match c2cloud resource managerment

--- build/rdo/apply_patch.sh
+++ build/rdo/apply_patch.sh
@@ -2,5 +2,11 @@
 
 yum install -y patch
 patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/floating_ips.py < ~/rdo/patches/allocate_specific_floating_ip.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/floating_ips_bulk.py < ~/rdo/patches/add_id_host_to_floating_ip.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/os_networks.py < ~/rdo/patches/get_tenantid_from_parameters.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/network/api.py < ~/rdo/patches/add_impl_allocate_floating_ip.patch
+patch -p1 /usr/lib/python2.7/site-packages/nova/network/base_api.py < ~/rdo/patches/add_interface_allocate_floating_ip.patch
 pkill -9 dnsmasq
 systemctl restart openstack-nova-network
