41eaa07ea715d28fb818198069800dea694edaee
tealover <pyz007@gmail.com>
2015-07-17 16:36:47 +0800

    add patch script to modify some python source code

--- build/rdo/apply_patch.sh
+++ build/rdo/apply_patch.sh
@@ -2,7 +2,6 @@
 
 yum install -y patch
 patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
-patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
 patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/floating_ips.py < ~/rdo/patches/allocate_specific_floating_ip.patch
 patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/floating_ips_bulk.py < ~/rdo/patches/add_id_host_to_floating_ip.patch
 patch -p1 /usr/lib/python2.7/site-packages/nova/api/openstack/compute/contrib/os_networks.py < ~/rdo/patches/get_tenantid_from_parameters.patch
@@ -10,3 +9,4 @@ patch -p1 /usr/lib/python2.7/site-packages/nova/network/api.py < ~/rdo/patches/a
 patch -p1 /usr/lib/python2.7/site-packages/nova/network/base_api.py < ~/rdo/patches/add_interface_allocate_floating_ip.patch
 pkill -9 dnsmasq
 systemctl restart openstack-nova-network
+systemctl restart openstack-nova-api
--- build/rdo/install.sh
+++ build/rdo/install.sh
@@ -173,11 +173,7 @@ function post_install() {
 
 function apply_patches() {
     yum install -y patch
-    patch -p1 /usr/lib/python2.7/site-packages/nova/network/linux_net.py < ~/rdo/patches/linux_net_br100_promisc.patch
-    pkill -9 dnsmasq
-    systemctl restart openstack-nova-network
-
-    patch -p1 /usr/lib/python2.7/site-packages/nova/scheduler/filters/aggregate_multitenancy_isolation.py < ~/rdo/patches/scheduler_filter_aggregate.patch
+    ./apply_patch.sh
     systemctl restart openstack-nova-scheduler
 }
 
